f02ec9c81d1ee9d091df5921584204713d7de2b4
BrandonDewell <bsdewell14@gmail.com>
2022-05-03 10:37:25 -0400

    test

---
+++ src/controller/MainMenuController.java
@@ -0,0 +1,43 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+// test
+
+public class MainMenuController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        System.out.println("Main Menu is initialized");
+    }
+
+    public void onActionAddCustomer(ActionEvent actionEvent) {
+    }
+
+    public void onActionUpdateCustomer(ActionEvent actionEvent) {
+    }
+
+    public void onActionDeleteCustomer(ActionEvent actionEvent) {
+    }
+
+    public void onActionAddAppointment(ActionEvent actionEvent) {
+    }
+
+    public void onActionUpdateAppointment(ActionEvent actionEvent) {
+    }
+
+    public void onActionDeleteAppointment(ActionEvent actionEvent) {
+    }
+
+    public void onActionWeekView(ActionEvent actionEvent) {
+    }
+
+    public void onActionMonthView(ActionEvent actionEvent) {
+    }
+
+    public void onActionAllView(ActionEvent actionEvent) {
+    }
+}
